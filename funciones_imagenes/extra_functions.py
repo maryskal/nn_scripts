f7b452b926c055e3814f32b14f5c9cc9737b9fcf
maryskal_gpus <meroll16@gmail.com>
2022-08-08 19:07:04 -0400

    cambios en simplificacion de .py

--- funciones_imagenes/extra_functions.py
+++ funciones_imagenes/extra_functions.py
@@ -1,5 +1,4 @@
 from tensorflow.keras import backend as K
-from tensorflow import keras
 import os
 
 def list_files(path):
@@ -17,11 +16,3 @@ def dice_coef(y_true, y_pred, smooth=100):
 def dice_coef_loss(y_true, y_pred):
     return 1 - dice_coef(y_true, y_pred)
 
-
-def charge_model(name, dice=False):
-    if dice:
-        mask_model = keras.models.load_model('/home/mr1142/Documents/Data/models/' + name + '.h5', 
-                                     custom_objects={"dice_coef_loss": dice_coef_loss, "dice_coef": dice_coef})
-    else:
-        mask_model = keras.models.load_model('/home/mr1142/Documents/Data/models/unet_1chan_1.h5')
-    return mask_model
